f6ef0d201d63c99e0f2c064b30d08fb988b5bb7d
pavloruban1 <137701866+pavloruban1@users.noreply.github.com>
2023-08-25 10:06:53 +0300

    1
    add f

---
+++ vue_flowers_landing/src/components/MyHeader.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <h1 class="col-12">S A D E F L O W E R</h1>
+      <h4>My Favorite Colors are flowers</h4>
+      <div class="col-12"><img src="src/assets/img/ico_phone.png" alt="1">+2349029132521</div>
+      <form class="col-12">
+        <div class="form-row">
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+        </div>
+      </form>
+      <div class="col-6">
+        <div class="div">
+          <img src="src/assets/img/ico_man.png" alt="1" class="mw-100">
+          <p>Login</p>
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="div">
+          <img src="src/assets/img/ico_bag.png" alt="1" class="mw-100">
+          <p>Cart</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="#">Special Offers</a>
+          <a class="navbar-brand" href="#">Birthday Flowers</a>
+          <a class="navbar-brand" href="#">Luxury Collection</a>
+          <a class="navbar-brand" href="#">Our Complete Range</a>
+          <a class="navbar-brand" href="#">Rose Collection</a>
+          <a class="navbar-brand" href="#">Events</a>
+        </div>
+      </nav>
+    </div>
+  </div>
+  <main>
+    <div class="container">
+      <section>
+        <h1>Luxury Pink Peonies Bouquet</h1>
+        <button class="btn btn-outline-success" type="button">SHOP NOW</button>
+      </section>
+      <section>
+        <h2>FRESH AND BEAUTIFUL FLOWERS</h2>
+        <div class="row">
+          <div class="col-3">
+            <img src="src/assets/img/flw_1.png" alt="1" class="mw-100">
+            <p>Sade Love</p>
+          </div>
+          <div class="col-3">
+            <img src="src/assets/img/flw_2.png" alt="1" class="mw-100">
+            <p>Marry Me</p>
+          </div>
+          <div class="col-3">
+            <img src="src/assets/img/flw_3.png" alt="1" class="mw-100">
+            <p>Send Love</p>
+          </div>
+          <div class="col-3">
+            <h3>Best Online Flower Shop In Nigeria</h3>
+            <h5>Read More</h5>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="row">
+          <div class="col-6">
+            <h2>NO.1 ORIGINAL FLOWER MARKET</h2>
+            <h4>Since 2016 we have been delivering across Nationwide with the freshest most beautiful flowers. Impeccable flowers at great prices, delivered quickly throughout Nigeria.</h4>
+            <button class="btn btn-secondary" type="button">SHOP NOW</button>
+          </div>
+          <div class="col-6">
+            <img src="src/assets/img/flw_4.png" alt="1" class="mw-100">
+          </div>
+        </div>
+      </section>
+      <section>
+        <h3>NEED SOME ASSISTANCE?</h3>
+        <h2>OUR CUSTOMER CARE TEAM IS HERE TO HELP</h2>
+        <p>Give us a call, or WhatsApp us on +234 902 931 2521. Our team would be delighted to talk you through the process and help you select the perfect flowers for your occasion.</p>
+      </section>
+      <section>
+        <h2>Sade Flower Shop</h2>
+        <h3>The Art Of Gifting</h3>
+      </section>
+
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "MyHeader"
+
+}
+</script>
+
+<style scoped>
+
+</style>
